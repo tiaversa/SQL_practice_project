cb3ac4b4d5ec59f984c2a4806dedf181b192ca72
tiaversa <timnaaversa@gmail.com>
2021-12-22 14:42:18 +0100

    Creating a docker image for th set up part of the process.

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
 from sqlalchemy import *
-import pymysql
+from connction_db import *
+from set_up import *
 
-
-def engine_connect_db():
-    engine = create_engine(
-        f'mysql+pymysql://{mysql_db_config["user"]}:{mysql_db_config["password"]}@{mysql_db_config["host"]}'
-    )  # connect to server
-
-    # check if database exists if not add the database
-    engine.execute(
-        f"CREATE DATABASE IF NOT EXISTS {mysql_db_config['database']};"
-    )  # create db
-
-    # adding the database to the engine call
-    engine = create_engine(
-        f'mysql+pymysql://{mysql_db_config["user"]}:{mysql_db_config["password"]}@{mysql_db_config["host"]}/{mysql_db_config["database"]}'
-    )
+def main():
+    # make the connection and add the database if necessary
+    engine = engine_connect_db()
+    # create the table and load information for queries into it.
+    creating_the_setup(engine)
     return engine
 
-
-mysql_db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '*Saltlake5',
-    'port': 3306,
-    'database' : 'linux_test'
-}
-
-engine = engine_connect_db()
-print(engine)
-print('hi')
+main()
